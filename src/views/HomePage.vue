08177e71f8e0f0f50ca1e2ec89efee6f7e204609
Kevin Liu <kliu@southtexascollege.edu>
2022-11-19 23:24:52 -0600

    image source fixed and jpg renamed

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -7,7 +7,7 @@ const title = ref('Fandom Forum Site')
   <main class="flex items-start justify-start">
     <h1 class="px-5 font-mono text-6xl font-bold text-black">{{ title }}</h1>
   </main>
-  <img class="image" img src="fandom.jpg" />
+  <img class="image" img src="/public/fandom.jpg" />
 </template>
 
 <style lang="postcss" scoped>
