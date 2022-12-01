cc60317758b6ea5a7925bb873abb336d74591049
Kevin Liu <kliu@southtexascollege.edu>
2022-12-01 06:30:49 -0600

    build and styled a book now page

---
+++ src/components/BookNow.vue
@@ -0,0 +1,19 @@
+<script setup></script>
+
+<template>
+  <div class="wrapper">
+    <h1>CHECK OUT WHAT WE DO IN THIS SERVER!</h1>
+  </div>
+  <div class="message">
+    <p>Please fill out the form below to schedule an appointment with us.</p>
+  </div>
+</template>
+
+<style lang="postcss" scoped>
+.wrapper {
+  @apply text-center text-3xl font-bold text-white;
+}
+.message {
+  @apply py-4 text-center text-base font-medium italic text-white;
+}
+</style>
