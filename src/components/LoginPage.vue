70d69edaab620ee0f3f368682cf7fbc993eb1706
Kevin Liu <kliu@southtexascollege.edu>
2022-12-04 22:42:39 -0600

    style change

--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -45,7 +45,7 @@ body {
   @apply items-center object-center py-10;
 }
 .login {
-  @apply mx-auto h-1/5 w-1/4 justify-center rounded-3xl bg-zinc-800 p-16 text-center;
+  @apply mx-auto h-1/5 w-1/4 justify-center rounded-3xl bg-zinc-900 p-16 text-center;
 }
 .title {
   @apply mx-auto items-center justify-center text-2xl font-semibold text-white;
@@ -63,6 +63,6 @@ body {
   @apply bg-transparent text-white underline;
 }
 .submit {
-  @apply h-14 w-full cursor-pointer rounded-lg bg-white font-medium hover:bg-red-500 hover:text-white hover:shadow-md hover:shadow-zinc-800;
+  @apply h-14 w-full cursor-pointer rounded-lg bg-white font-medium hover:bg-red-500 hover:text-white hover:shadow-md hover:shadow-zinc-900;
 }
 </style>
