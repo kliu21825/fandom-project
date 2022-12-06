f58020476ba1aeecf43c6cf17126c29b7d2513f1
Kevin Liu <kliu@southtexascollege.edu>
2022-12-05 19:37:11 -0600

    a lot of changes

--- src/components/BookNow.vue
+++ src/components/BookNow.vue
@@ -10,25 +10,29 @@
   <body>
     <div class="appointment">
       <div class="title">Book Now</div>
-      <form class="appointment-form" @submit.prevent="logUserIn">
+      <form class="appointment-form">
         <div class="textbox">
-          <input v-model="username" type="text" placeholder="Address" />
+          <input v-model="address" type="text" placeholder="Address" />
         </div>
         <div class="textbox">
-          <input v-model="password" type="text" placeholder="Phone Number" />
+          <input v-model="phone" type="tel" placeholder="Phone Number" />
         </div>
-        <div class="dropdown">
-          <label for="cars">Select vehicle type: </label>
-          <div class="dropdown-content">
-            <select id="cars" name="cars">
+        <div class="date">
+          <input v-model="date" type="date" />
+        </div>
+
+        <div class="dropdown-content">
+          <p class="vehicle">
+            Select vehicle type:
+            <select id="cars" class="cars">
               <option value="sedan">Sedan</option>
               <option value="suv">SUV</option>
               <option value="truck">Truck</option>
               <option value="van">Van</option>
             </select>
-          </div>
+          </p>
+          <button type="submit" class="submit">Submit</button>
         </div>
-        <button type="submit" class="submit">Submit</button>
       </form>
     </div>
   </body>
@@ -42,13 +46,13 @@
   @apply py-4 text-center text-base font-medium italic text-white;
 }
 body {
-  @apply items-center object-center py-6;
+  @apply items-center object-center;
 }
 .appointment {
   @apply mx-auto h-1/5 w-1/3 justify-center rounded-3xl bg-zinc-900 p-12 text-center;
 }
 .title {
-  @apply mx-auto items-center justify-center py-2 text-2xl font-semibold text-white;
+  @apply mx-auto items-center justify-center text-2xl font-semibold text-white;
 }
 .appointment-form {
   @apply m-0 grid w-full place-items-center gap-2;
@@ -56,13 +60,37 @@ body {
 .textbox {
   @apply relative h-14 rounded-lg border-white;
 }
-.dropdown {
+.dropdown-content {
+  @apply text-black;
+}
+.vehicle {
   @apply text-white;
 }
-.dropdown-content {
+.cars {
   @apply text-black;
 }
 .submit {
   @apply h-14 w-full cursor-pointer rounded-lg bg-red-500 font-medium hover:bg-white hover:text-black hover:shadow-md hover:shadow-zinc-900;
+}
+.btn {
+  @apply h-4 w-10 cursor-pointer  rounded-t-3xl bg-slate-500 p-2 text-xl font-medium outline-none;
+}
+.popup {
+  @apply absolute top-1/2 left-2/4 mt-10 w-96 translate-x-2/4 translate-y-2/4 rounded-md bg-white;
+}
+.container {
+  @apply top-1/2 h-auto w-80 rounded-md;
+}
+#click {
+  @apply hidden;
+}
+#click:checked ~ .container {
+  @apply visible;
+}
+.popup-img {
+  @apply m-11 w-24 place-items-center justify-center rounded-lg;
+}
+.thanks {
+  @apply text-center text-4xl font-medium;
 }
 </style>
